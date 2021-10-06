2d52c11eb67d790e5a51da4d26efbc6f771332af
matloff <matloff@cs.ucdavis.edu>
2021-10-05 20:12:23 -0700

    finally, adult data OK

--- Data/Adult.R
+++
@@ -1,8 +0,0 @@
-
-# famous Adult dataset, some preprocessing
-
-adult <- read.csv('~/Datasets/Adult/adult.data',stringsAsFactors=T)
-names(adult) <- c('age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country')
-adult$fnlwgt <- NULL
-adult$'education-num' <- NULL
-
