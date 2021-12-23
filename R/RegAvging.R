ee02fd713d1ecb764f1079372bab0b8d4a369c1e
matloff <matloff@cs.ucdavis.edu>
2021-12-22 23:45:17 -0800

    working, interesting results for German Credit (Gender) and COMPAS[,c(1:8,10)] (race)

--- R/RegAvging.R
+++ R/RegAvging.R
@@ -8,11 +8,36 @@
 
 regAvg <- function(data,yName,qeFtn,grpName) 
 {
-   grps <- split(data,data[['grpName']]
-   qeObjs <- lapply(grps,
-      function(grp) qeFtn(grp$data,yName)
+   grps <- split(data,data[[grpName]])
+   # get XY data by removing grouping variable
+   grpsXY <- lapply(grps,function(grp) {grp[[grpName]] <- NULL; grp})
+   # get X data by removing Y variable
+   grpsX <- lapply(grpsXY,function(grp) {grp[[yName]] <- NULL; grp})
+   # do the model fits
+   qeObjs <- lapply(grpsXY,
+      function(grp) qeFtn(grp,yName,holdout=NULL))
 
+   classif <- is.factor(data[[yName]])
+   nGrps <- length(grps)
+   avgs <- matrix(nrow=nGrps,ncol=nGrps)
+   rownames(avgs) <- levels(data[[grpName]])
+   colnames(avgs) <- levels(data[[grpName]])
 
-   predict(qeObj,newxDF) / nrow(newxDF))
+   for (i in 1:nGrps)
+      for (j in 1:nGrps) {
+         if (i == j) {
+            # EY = E[E(Y|X)]
+            tmp <- grps[[i]][[yName]]
+            if (classif) tmp <- as.numeric(tmp) - 1
+            avgs[i,i] <- mean(tmp)
+         } else {
+            grpsxj <- grpsX[[j]]
+            tmp <- predict(qeObjs[[i]],grpsxj)
+            if (classif) tmp <- tmp$probs[,1]
+            avgs[i,j] <- mean(tmp)
+         }
+      }
+   
+   avgs
 }
 
