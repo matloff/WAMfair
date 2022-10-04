3368c344d1349d43397d49f4f4fbeb10960dc08a
Norm Matloff <nsmatloff@ucdavis.edu>
2022-10-04 14:28:51 -0700

    now check std err formulas, as numbers seem large

--- R/Utils.R
+++ R/Utils.R
@@ -2,6 +2,13 @@
 # to set up stdErr with qeLin, make all variables numeric except Y and
 # the group variable
 
+# args:
+
+#    data: data frame, usual qeML form
+#    yName: name of Y, usual qeML form; must be an R factor for
+#       classification case
+#    grpName: must be an R factor
+
 makeAllXNumeric <- function(data,yName,grpName)
 {
    yCol <- which(names(data) == yName)
@@ -14,8 +21,15 @@ makeAllXNumeric <- function(data,yName,grpName)
    nColRes <- ncol(res)
    colnames(res)[(nColRes-1):nColRes] <- c(grpName,yName)
 
-   xd <- as.data.frame(xd)
+   res <- as.data.frame(res)
+
+   if (is.factor(data[[yName]]))
+      res[,nColRes] <- as.factor(res[,nColRes])
+   res[,nColRes-1] <- as.factor(res[,nColRes-1])
 
    res
+
 }
+
+mAXN <- makeAllXNumeric
 
